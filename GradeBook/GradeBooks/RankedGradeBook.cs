157d572e0dcc836c7f31d2973630b6a451218ca5
Torgeir <torgeipt@users.noreply.github.com>
2018-08-20 16:42:15 +0200

    Added calculatestatistics in rankedgradebook

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -11,6 +11,26 @@ namespace GradeBook.GradeBooks
             this.Type = GradeBookType.Ranked;
         }
 
+        public override void CalculateStudentStatistics(string name) {
+
+            if(Students.Count < 5) {
+                Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+                return;
+            }
+
+            base.CalculateStudentStatistics(name);
+        }
+
+        public override void CalculateStatistics() {
+
+            if(Students.Count < 5) {
+                Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+                return;
+            }
+
+            base.CalculateStatistics();
+        }
+
         public override char GetLetterGrade(double averageGrade) {
             int numStudents = Students.Count;
 
